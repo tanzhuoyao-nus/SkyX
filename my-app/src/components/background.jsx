c1e0c1d0996dc5b7227a7e1504ad875a62325077
Zhang Anli <e0415510@u.nus.edu>
2020-05-24 17:31:09 +0800

    html version 1

--- my-app/src/components/background.jsx
+++ my-app/src/components/background.jsx
@@ -5,7 +5,7 @@ import background from '../images/background.jpeg';
 console.log(background); 
 
 function Background (){ 
-    return <img src={background} alt="Logo" width="100%" height="500px" /> 
+    return <img src={background} alt="Background" width="100%" /> 
 }
 
 export default Background; 
