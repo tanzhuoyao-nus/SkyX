35d1debd7d1e37468a0814efb00a528e695c2caa
tanzhuoyao-nus <65391538+tanzhuoyao-nus@users.noreply.github.com>
2020-05-24 20:25:32 +0800

    Fixed the naming

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './index.css'; 
 
 //component imports 
-import Chart from './components/Chart';
-import Logo from './components/Logo'; 
-import Navbar from './components/Navbar';
+import Chart from './components/chart';
+import Logo from './components/logo'; 
+import Navbar from './components/navbar';
 
 
 
@@ -13,22 +13,9 @@ function App() {
     <div>
       <Navbar/>
       <Logo />   
-
       <h1>Welcome to SkyX</h1>
       <input id="form" placeholder="Your name here"></input>
-
-
       <Chart /> 
-
-
-
-
-
-
-
-
-
-      
     </div>
   );
 }
