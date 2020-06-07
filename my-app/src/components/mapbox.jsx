f4f7124d1cdace9e8603ad6b10556b58ebc95e93
Zhang Anli <e0415510@u.nus.edu>
2020-06-07 14:40:59 +0800

    2.40pm

--- my-app/src/components/mapbox.jsx
+++ my-app/src/components/mapbox.jsx
@@ -6,8 +6,8 @@ class Map extends Component {
 
     state = {
       viewport: {
-        width: "80vw",
-        height: "50vh",
+        width: "100vw",
+        height: "60vh",
         latitude: 1.3521,
         longitude: 103.8198,
         zoom: 5
@@ -17,9 +17,21 @@ class Map extends Component {
     render() {
       return (
         <ReactMapGL 
-          {...this.state.viewport} mapboxApiAccessToken={"pk.eyJ1IjoiYW5sZWVlIiwiYSI6ImNrYjM0MmJ1aDBoYWQyeGs2cm43bGlqdzUifQ.QsauNU7FFlsWYn-HbF2D8w"}
+          {...this.state.viewport} 
+          mapboxApiAccessToken={"pk.eyJ1IjoiYW5sZWVlIiwiYSI6ImNrYjM0MmJ1aDBoYWQyeGs2cm43bGlqdzUifQ.QsauNU7FFlsWYn-HbF2D8w"}
+          mapStyle="mapbox://styles/anleee/ckb4n5bon1pct1is727fqbnpl"
           onViewportChange={(viewport) => this.setState({viewport})}
-        />
+        >
+            {/* Marker */}
+            {/* <Marker lattitude={51.5074} longitude={0.1278}> 
+                <button>
+                    <img src="https://www.pngguru.com/free-transparent-background-png-clipart-yfuav" />
+                </button>
+            </Marker> */}
+            
+
+
+        </ReactMapGL>
       );
     }
   }
