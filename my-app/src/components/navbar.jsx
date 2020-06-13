9aad39bdcc6567577b1c65b8f9d22a34d9ceab4d
tanzhuoyao-nus <65391538+tanzhuoyao-nus@users.noreply.github.com>
2020-06-13 23:33:00 +0800

    Fixed bug

--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -18,7 +18,7 @@ function Navbar (){
         <div className="menu__right">
             <ul className="menu__list">
                 <li className="menu__list-item"><a className="menu__link menu__link--active" href="/">Home</a></li>
-                <li class="sub-menu" className="menu__list-item"><a className="menu__link" href="/pricechart">Price Chart</a></li>
+                <li className="sub-menu menu__list-item"><a className="menu__link" href="/pricechart">Price Chart</a></li>
                 <li className="menu__list-item"><a className="menu__link" href="/pricemap">Price Maps</a></li>
                 <li className="menu__list-item"><a className="menu__link" href="/pricecomparison">Price Comparison</a></li>
                 <li className="menu__list-item"><a className="menu__link" href="/buyorder">Price Alert</a></li>
