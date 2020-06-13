3f3b8f1568e5849c004fb4c96155a6d7fb9c2f7d
Zhang Anli <e0415510@u.nus.edu>
2020-06-13 18:55:21 +0800

    updated

--- my-app/src/components/chartdatacards.jsx
+++ my-app/src/components/chartdatacards.jsx
@@ -4,57 +4,114 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import {Grid} from '@material-ui/core'; 
+import { db } from './firebase';
 
+function cityPicker(city) {
+    if (city === "Tokyo") {
+      return "TYO";
+    } else if (city === "Bali") {
+      return "DPS";
+    } else if (city=== "London") {
+      return "LON";
+    } else if (city === "Hong Kong") {
+      return "HKG";
+    } else {
+      return "KUL";
+    }
+  }
+
+function convert2dp(arr){
+  var newArr = [];
+  for(let i=0; i<arr.length; i++){
+      newArr.push(arr[i].toFixed(2));
+  }
+  return newArr;
+}
+
+async function getFirebaseData(city) {
+    const document = await db.collection('flight_price_' + city).doc("Prices").get();
+    const all_time_average = await document.get("All Time Average"); 
+    const all_time_high = await document.get("All Time High");
+    const all_time_low = await document.get("All Time Low");
+    var output_arr = [];
+    output_arr[0]= convert2dp(all_time_average); 
+    output_arr[1]= convert2dp(all_time_high); 
+    output_arr[2]= convert2dp(all_time_low); 
+    return output_arr;
+}
 
 const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+      width:700, 
+    },
 });
 
-export default function OutlinedCard() {
-  const classes = useStyles();
+export default class OutlinedCard extends React.Component {
+    const classes = useStyles();
 
-  return (
-    <Grid id ="card" container spacing={3} justify="center">
-        <Card className={classes.root} variant="outlined">
-            
-            <CardContent>
-                <Typography variant="h5" component="h2">
-                Average Price: 
-                </Typography>
-            </CardContent>
+    constructor()
+    { 
+        super(); 
+        console.warn("constructor"); 
+        this.state={ 
+            data: [], 
+        }; 
+    }
+  
+  async componentDidMount() { 
+    const city = cityPicker(this.props.name);
+    var date_arr = [];
+    var final_prices = await getFirebaseData(city).catch(err => console.log("Oops error"));
+    this.setState({ datasets: 
+        [
+          {
+            data : final_prices[0]
+          },
+          {
+            data : final_prices[1]
+          }, 
+          {
+            data : final_prices[2]
+          }
+        ]
+      })
+  }
 
-        </Card>
-        <Card className={classes.root} variant="outlined">
-            
-            <CardContent>
-                <Typography variant="h5" component="h2">
-                Highest Price: 
-                </Typography>
-            </CardContent>
+  render(){
+    return (
+        <Grid id ="card" container spacing={3} justify="center">
+            <Card className={classes.root} variant="outlined">
+                
+                <CardContent>
+                    <Typography variant="h5" component="h2">
+                    Average Price: {this.state} 
+                    </Typography>
+                </CardContent>
 
-        </Card>
-        <Card className={classes.root} variant="outlined">
-            
-            <CardContent>
-                <Typography variant="h5" component="h2">
-                Lowest Price: 
-                </Typography>
-            </CardContent>
+            </Card>
+            <Card className={classes.root} variant="outlined">
+                
+                <CardContent>
+                    <Typography variant="h5" component="h2">
+                    Highest Price: {this.state} 
+                    </Typography>
+                </CardContent>
 
-        </Card>
-    </Grid> 
-  );
+            </Card>
+            <Card className={classes.root} variant="outlined">
+                
+                <CardContent>
+                    <Typography variant="h5" component="h2">
+                    Lowest Price: {this.state} 
+                    </Typography>
+                </CardContent>
+
+            </Card>
+        </Grid>
+        );
+    } 
 }
