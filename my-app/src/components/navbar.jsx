670e6e6467a03c817ce39a84573d8baa368f59d2
Zhang Anli <e0415510@u.nus.edu>
2020-07-13 09:57:13 +0800

    Minor HTML UI Edits

--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'; 
 import logo from '../images/navbarlogo.png'; 
-import searchIcon from '../images/search.png'; 
 import './components.css'; 
 
 
@@ -24,15 +23,6 @@ function Navbar (){
                 <li className="menu__list-item"><a className="menu__link" href="/buyorder">Price Alert</a></li>
             </ul>
 
-            <button style ={{
-
-                backgroundImage:'url(' + searchIcon + ')'
-                
-            }} className="menu__search-button"></button>
-
-            <form className="menu__search-form hide" method="POST">
-                <input className="menu__search-input" placeholder="Type and hit enter" />
-            </form>
         </div>
     </nav>
 
